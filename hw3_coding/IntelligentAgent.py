7eb7e95a209f4cd6f8e9c482ed566e3d06e67806
sunnysarkar <sarkarsunny29@gmail.com>
2025-11-05 18:42:50 -0800

    start with in class minmax alg

---
+++ hw3_coding/IntelligentAgent.py
@@ -0,0 +1,84 @@
+import random
+from BaseAI import BaseAI
+
+import time
+
+vecIndex = [UP, DOWN, LEFT, RIGHT] = range(4)
+# getAvailableMoves(self, dirs=vecIndex): # -> List[(int, Grid)]
+
+class IntelligentAgent(BaseAI):
+    maxTime = 0.2   # Maximum time allowed per move in seconds
+    buffer  = 0.01  # Buffer time to make sure we don't exceed maxTime
+    turnStartTime = None
+
+    def minimize(self, grid, alpha, beta):
+        minUtility = float('inf')
+        minChild = None
+        minMove = None
+
+        if terminalTest(grid):
+            return None, eval(grid), None
+        
+        for move, newGrid in grid.getAvailableMoves():
+            _, utility = self.maximize(newGrid, alpha, beta)
+
+            if utility < minUtility:
+                minChild, minUtility, minMove = newGrid, utility, move
+
+            if minUtility <= alpha:
+                break
+
+            if minUtility < beta:
+                beta = minUtility
+
+        return minChild, minUtility, minMove
+
+    def maximize(self, grid, alpha, beta):
+        maxUtility = float('-inf')
+        maxChild = None
+        maxMove = None
+
+        if terminalTest(grid):
+            return None, eval(grid), None
+        
+        for move, newGrid in grid.getAvailableMoves():
+            _, utility = self.maximize(newGrid, alpha, beta)
+
+            if utility > maxUtility:
+                maxChild, maxUtility, minmaxMoveMove = newGrid, utility, move
+
+            if maxUtility <= alpha:
+                break
+
+            if maxUtility < beta:
+                beta = maxUtility
+
+        return maxChild, maxUtility, maxMove
+    
+
+
+
+    def getMove(self, grid):
+        startTime = time.process_time()
+
+        # Implement iterative deepening here
+        
+        
+
+
+        return random.choice(moveset)[0] if moveset else None
+
+    # def getMoveWithIds(self, grid):
+    #     startTime = time.process_time()
+
+    #     # Implement iterative deepening here
+    #     depth = 1
+    #     while time.process_time() - startTime < self.maxTime - self.buffer:
+    #         availableMoves, resultGrid = grid.getAvailableMoves()
+
+    #         for 
+        
+
+
+    #     return random.choice(moveset)[0] if moveset else None
+        
